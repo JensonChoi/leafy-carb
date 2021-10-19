90cb9539eade9ee48d29945d739a077d43c15dc2
JensonChoi <jenson.choi@gmail.com>
2021-10-18 22:40:59 -0700

    Implemented training loop

--- train_functions/starting_train.py
+++ train_functions/starting_train.py
@@ -30,35 +30,70 @@ def starting_train(
         val_dataset, batch_size=batch_size, shuffle=True
     )
 
+    # Use GPU if available
+    if torch.cuda.is_available(): # Check if GPU is available
+        device = torch.device('cuda')
+    else:
+        device = torch.device('cpu')
+    # Move the model to the GPU
+    model.to(device)
+
     # Initalize optimizer (for gradient descent) and loss function
     optimizer = optim.Adam(model.parameters())
     loss_fn = nn.CrossEntropyLoss()
 
     # Initialize summary writer (for logging)
+    writer = None
     if summary_path is not None:
         writer = torch.utils.tensorboard.SummaryWriter(summary_path)
 
     step = 0
+    train_loss, n_correct = 0, 0
+    count = 0
     for epoch in range(epochs):
         print(f"Epoch {epoch + 1} of {epochs}")
 
         # Loop over each batch in the dataset
         for i, batch in enumerate(train_loader):
             print(f"\rIteration {i + 1} of {len(train_loader)} ...", end="")
+            # make prediction and compute loss
+            images, labels = batch
+            pred = model(images)
+            loss = loss_fn(pred, labels)
+
+            # update train_loss, n_correct, count
+            train_loss += loss
+            n_correct += compute_ncorrect(torch.argmax(pred, dim=1), labels)
+            count += len(labels)
 
             # TODO: Backpropagation and gradient descent
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
 
             # Periodically evaluate our model + log to Tensorboard
             if step % n_eval == 0:
                 # TODO:
                 # Compute training loss and accuracy.
                 # Log the results to Tensorboard.
+                train_accuracy = n_correct / count
+                if writer:
+                    writer.add_scalar('Loss/train', train_loss, epoch)
+                    writer.add_scalar('Accuracy/train', train_accuracy, epoch)
+
+                # reset train_loss, n_correct, count
+                train_loss, training_accuracy = 0, 0
+                count = 0
 
                 # TODO:
                 # Compute validation loss and accuracy.
                 # Log the results to Tensorboard.
                 # Don't forget to turn off gradient calculations!
-                evaluate(val_loader, model, loss_fn)
+                val_loss, val_acc = evaluate(val_loader, model, loss_fn)
+                if writer:
+                    writer.add_scalar('Loss/validation', val_loss, epoch)
+                    writer.add_scalar('Accuracy/validation', val_acc, epoch)
+                model.train()
 
             step += 1
 
@@ -81,6 +116,12 @@ def compute_accuracy(outputs, labels):
     n_total = len(outputs)
     return n_correct / n_total
 
+def compute_ncorrect(outputs, labels):
+    """
+    Computes the number of correct predictions that the model makes.
+    """
+    n_correct = (outputs == labels).sum().item()
+    return n_correct
 
 def evaluate(val_loader, model, loss_fn):
     """
@@ -88,4 +129,16 @@ def evaluate(val_loader, model, loss_fn):
 
     TODO!
     """
-    pass
+    model.eval()
+    total_loss, n_correct = 0, 0
+    count = 0
+    for i, batch in enumerate(val_loader):
+        # make prediction and compute loss
+        images, labels = batch
+        pred = model(images)
+        total_loss += loss_fn(pred, labels)
+        n_correct += compute_ncorrect(torch.argmax(pred, dim=1), labels)
+        count += len(labels)
+
+    accuracy = n_correct / count
+    return total_loss, accuracy
