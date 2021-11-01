1d9390ef8541512dd2a6d0c5e99bcce1743181fd
Aman Oberoi <amanbir5103@gmail.com>
2021-11-01 16:36:45 -0700

    added a third conv layer

--- networks/StartingNetwork.py
+++ networks/StartingNetwork.py
@@ -13,9 +13,11 @@ class StartingNetwork(torch.nn.Module):
         self.pool1 = nn.MaxPool2d(2, stride=2)
         self.conv2 = nn.Conv2d(6, 16, kernel_size=3, padding=1)
         self.pool2 = nn.MaxPool2d(2, stride=2)
+        self.conv3 = nn.Conv2d(16, 48, kernel_size=3, padding=1)
+        self.pool3 = nn.MaxPool2d(2, stride=2)
         self.relu = nn.ReLU()
         self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(56 * 56 * 16, 256)
+        self.fc1 = nn.Linear(28 * 28 * 48, 256)
         self.fc2 = nn.Linear(256, 5)
 
     def forward(self, x):
@@ -23,6 +25,8 @@ class StartingNetwork(torch.nn.Module):
         x = self.pool1(x)
         x = self.relu(self.conv2(x))
         x = self.pool2(x)
+        x = self.relu(self.conv3(x))
+        x = self.pool3(x)
         x = self.flatten(x)
         x = self.relu(self.fc1(x))
         x = self.fc2(x)
